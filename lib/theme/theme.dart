bae74a16c04c76a01af2acfde4b43a834ba2ed1f
SAQG007 <syedabdulqadirgillani807@gmail.com>
2023-08-13 19:55:16 +0500

    Created theme

---
+++ lib/theme/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+ThemeData myTheme = ThemeData(
+  useMaterial3: true,
+  brightness: Brightness.light,
+  textTheme: const TextTheme(
+    bodySmall: TextStyle(
+      color: Colors.white,
+    ),
+    bodyMedium: TextStyle(
+      color: Colors.white,
+    ),
+    bodyLarge: TextStyle(
+      color: Colors.white,
+    ),
+  ),
+);
